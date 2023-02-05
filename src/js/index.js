6b44c23424e59f8a10c33ea061fa3c7ee693a51f
deh3nne <contact@deviantdev.com>
2023-02-05 15:51:53 +0100

    Try to adjust timings again

--- src/js/index.js
+++ src/js/index.js
@@ -21,15 +21,15 @@ thirdCanvas.setAttribute("style","display:none");
 let containerToggleOne = setTimeout(function(){
 	firstSlideContainer.setAttribute("style","display:none");
 	secondSlideContainer.setAttribute("style","display:block");
-},5000);
+},2500);
 
 
 let removeFirstSlide = setTimeout(function(){
 	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
 	secondCanvas.setAttribute("style","display:block");
-},10000);
+},6500);
 
 let removeSecondCanvas = setTimeout(function(){
 	secondCanvas.setAttribute("style","display:none");
 	thirdCanvas.setAttribute("style","display:block")
-},15000)
+},9800)
